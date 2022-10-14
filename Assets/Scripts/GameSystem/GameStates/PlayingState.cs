4c18c98b1d5de85a6a29009953daa3370db7c026
Steven Verborgh <steven@verborgh.be>
2022-10-14 10:58:48 +0200

    Added additive scene loading for Gameplay and Menu

--- Assets/Scripts/GameSystem/GameStates/PlayingState.cs
+++ Assets/Scripts/GameSystem/GameStates/PlayingState.cs
@@ -1,33 +1,38 @@
 ﻿using BoardSystem;
+using GameSystem.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameSystem.GameStates
 {
     internal class PlayingState : GameState
     {
         public const string Name = "Playing";
+        private BoardView _boardView;
 
         public override void OnEnter()
         {
-            base.OnEnter();
-            Debug.Log("Entering Playing");
+            var loading = SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+            loading.completed += OnSceneLoaded;
         }
-
 
         public override void OnExit()
         {
-            base.OnEnter();
-            Debug.Log("Exiting Playing");
+            SceneManager.UnloadSceneAsync(2);
         }
 
-        public override void Select(Position position)
+        private void OnSceneLoaded(AsyncOperation obj)
         {
-            StateMachine.MoveTo(MenuState.Name);
+            _boardView = GameObject.FindObjectOfType<BoardView>();  
+            if(_boardView != null)
+            {
+                _boardView.PositionSelected += (s,e) => StateMachine.MoveTo(MenuState.Name);
+            }
         }
     }
 }
---
+++ Assets/Scripts/GameSystem/Views/MenuView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameSystem.Views
+{
+    internal class MenuView : MonoBehaviour
+    {
+        public event EventHandler StartClicked;
+
+        [SerializeField]
+        private Button _startButton;
+
+        private void Start()
+        {
+            _startButton.onClick.AddListener(() => OnStartClicked(EventArgs.Empty));
+        }
+
+        public void Show() => gameObject.SetActive(true);
+
+        public void Hide() => gameObject.SetActive(false);
+
+        protected virtual void OnStartClicked(EventArgs eventArgs)
+        {
+            var handler = StartClicked;
+            handler?.Invoke(this, eventArgs);
+        }
+    }
+}
